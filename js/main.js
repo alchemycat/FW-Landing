021e4a77cb1cdbe69258b93872e59f823eae7b47
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-04-28 16:28:47 +0300

    добавил адаптив для изображения в секции intro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const swiper = new Swiper(
+    function isWebp() {
+        function testWebP(callback) {
+            var webP = new Image();
+            webP.onload = webP.onerror = function () {
+                callback(webP.height == 2);
+            };
+            webP.src =
+                "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+    
+        testWebP(function (support) {
+            let className = support = true ? 'webp' : 'no-webp';
+            document.documentElement.classList.add(className)
+        });
+    }
+
+    isWebp();
+
+    new Swiper(
         ".swiper", {
             navigation: {
                 nextEl: '.navigation__button-next',
