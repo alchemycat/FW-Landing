aec4acb465d11e32d290fb149e45d0e9af514857
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-04-28 12:42:26 +0300

    добавил swiper

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper(
+        ".swiper", {
+            navigation: {
+                nextEl: '.navigation__button-next',
+                prevEl: '.navigation__button-prev',
+            },
+        }
+    );
+
+    console.log(swiper)
+
     function menu() {
         const burger = document.querySelector(".header__burger");
         const header = document.querySelector(".header__wrapper");
