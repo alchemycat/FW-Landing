51ff47e09bc5b72ebf0b34e1bcb75d5fae91b709
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-04-29 11:12:57 +0300

    добавил калькулятор

--- js/main.js
+++ js/main.js
@@ -1,40 +1,98 @@
 window.addEventListener("DOMContentLoaded", () => {
-    function isWebp() {
-        function testWebP(callback) {
-            var webP = new Image();
-            webP.onload = webP.onerror = function () {
-                callback(webP.height == 2);
-            };
-            webP.src =
-                "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	function isWebp() {
+		function testWebP(callback) {
+			var webP = new Image();
+			webP.onload = webP.onerror = function () {
+				callback(webP.height == 2);
+			};
+			webP.src =
+				"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+		}
+
+		testWebP(function (support) {
+			let className = (support = true ? "webp" : "no-webp");
+			document.documentElement.classList.add(className);
+		});
+	}
+
+	isWebp();
+
+	new Swiper(".swiper", {
+		navigation: {
+			nextEl: ".navigation__button-next",
+			prevEl: ".navigation__button-prev",
+		},
+	});
+
+	function menu() {
+		const burger = document.querySelector(".header__burger");
+		const header = document.querySelector(".header__wrapper");
+
+		burger.addEventListener("click", () => {
+			header.classList.toggle("active");
+			burger.classList.toggle("active");
+		});
+	}
+
+	menu();
+
+	function calc() {
+		const daysWrap = document.querySelector("[data-id='days']"),
+			hoursWrap = document.querySelector("[data-id='hours']"),
+            summWrap = document.querySelector(".summ");
+
+		let currentDayValue = 6;
+		let currentHoursValue = 6000;
+
+        const activeDaysItem = daysWrap.querySelector(`[data-value="${currentDayValue}"]`);
+        const activeHoursItem = hoursWrap.querySelector(`[data-value="${currentHoursValue}"]`);
+        
+        addActive(activeDaysItem, daysWrap, "days");
+        addActive(activeHoursItem, hoursWrap, "hours");
+        multiply(currentDayValue, currentHoursValue);
+
+		daysWrap.addEventListener("click", (e) => {
+			const target = e.target;
+
+			addActive(target, daysWrap, "days");
+            multiply(currentDayValue, currentHoursValue);
+
+		});
+
+		hoursWrap.addEventListener("click", (e) => {
+			const target = e.target;
+
+			addActive(target, hoursWrap, "hours");
+            multiply(currentDayValue, currentHoursValue);
+		});
+
+		function addActive(target, elementsContainer, type) {
+			if (target.classList.contains("calc__option")) {
+				if (type === "days") {
+					currentDayValue = +target.getAttribute("data-value");
+				} else {
+					currentHoursValue = +target.getAttribute("data-value");
+				}
+
+				removeActive(elementsContainer.childNodes);
+				target.classList.add("active");
+			}
+		}
+
+		function removeActive(row) {
+			row.forEach((element) => {
+				if (element.tagName === "BUTTON") {
+					element.classList.remove("active");
+				}
+			});
+		}
+
+        function multiply(a, b, c = 4) {
+            let result = a * b * c;
+			result = result.toLocaleString('ru-RU');
+            summWrap.textContent = result;
         }
-    
-        testWebP(function (support) {
-            let className = support = true ? 'webp' : 'no-webp';
-            document.documentElement.classList.add(className)
-        });
-    }
+	}
 
-    isWebp();
-
-    new Swiper(
-        ".swiper", {
-            navigation: {
-                nextEl: '.navigation__button-next',
-                prevEl: '.navigation__button-prev',
-            },
-        }
-    );
-
-    function menu() {
-        const burger = document.querySelector(".header__burger");
-        const header = document.querySelector(".header__wrapper");
-
-        burger.addEventListener("click", () => {
-            header.classList.toggle("active");
-            burger.classList.toggle("active");
-        })
-    }
-
-    menu();
+	calc();
 });
